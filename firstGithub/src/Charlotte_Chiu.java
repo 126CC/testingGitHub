d3ac5293f8ab3f69c8f0f16b41aac0026756b721
126CC <113187762+126CC@users.noreply.github.com>
2024-02-20 12:52:51 -0500

    Revert "Age"
    This reverts commit 9d436af53f7d0c0055b16341fb8ad1c405674ef5.

--- firstGithub/src/Charlotte_Chiu.java
+++ firstGithub/src/Charlotte_Chiu.java
@@ -2,8 +2,5 @@ public class Charlotte_Chiu {
     public static void main(String[] args) {
         System.out.println("Hello Charlotte");
         System.out.println("Hello other editor: ");
-
-        for (int i = 0; i < 15; i++)
-            System.out.println("I'm " + i + " years old");
     }
 }
