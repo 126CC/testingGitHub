7505ea0731cf7f516a27646ef336502768dd249b
Dan-Dempsey <113116053+Dan-Dempsey@users.noreply.github.com>
2024-02-20 12:19:44 -0500

    Hello

--- firstGithub/src/Charlotte_Chiu.java
+++ firstGithub/src/Charlotte_Chiu.java
@@ -1,3 +1,6 @@
 public class Charlotte_Chiu {
-    
+    public static void main(String[] args) {
+        System.out.println("Hello Charlotte");
+        System.out.println("Hello other editor: ");
+    }
 }
