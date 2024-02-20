9d436af53f7d0c0055b16341fb8ad1c405674ef5
Dan-Dempsey <113116053+Dan-Dempsey@users.noreply.github.com>
2024-02-20 12:21:13 -0500

    Age

--- firstGithub/src/Charlotte_Chiu.java
+++ firstGithub/src/Charlotte_Chiu.java
@@ -2,5 +2,8 @@ public class Charlotte_Chiu {
     public static void main(String[] args) {
         System.out.println("Hello Charlotte");
         System.out.println("Hello other editor: ");
+
+        for (int i = 0; i < 15; i++)
+            System.out.println("I'm " + i + " years old");
     }
 }
